aa3daef293b4abc25a9e1e3679f3b0aecc99d044
ttoghere <tuncsoftwarehere@gmail.com>
2023-05-05 13:25:29 +0300

    important pubs
    Pubs:
    1)Geolocatıor
    2)google_maps_flutter
    3)firebase

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyD3jONC01yeNiocqmNVr63uF5QqENL9P5c")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
