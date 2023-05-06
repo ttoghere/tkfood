4d2a157fd30b410f3dfa20ff6d6816c35b5e8251
ttoghere <tuncsoftwarehere@gmail.com>
2023-05-06 22:18:47 +0300

    What i did ?? && Next on:
    Did:
    Error solved by changing api key from only ios apps to none and it works now
    Next on:
    UI works

--- lib/repositories/places/places_repository.dart
+++ lib/repositories/places/places_repository.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:http/http.dart' as http;
 import 'package:tkfood/models/place_autocomplete_model.dart';
 import 'package:tkfood/models/place_model.dart';
@@ -7,15 +8,19 @@ import 'package:tkfood/repositories/places/base_places_repository.dart';
 class PlacesRepository extends BasePlacesRepository {
   final String types = "geocode";
   final String androidMapsAPI = "AIzaSyByxhuIcCMjdV7T9dAB2P7iBMDLKCu7cjs";
-  final String iosMapsAPI = "AIzaSyD3jONC01yeNiocqmNVr63uF5QqENL9P5c";
+  final String mapsAPI = "AIzaSyD3jONC01yeNiocqmNVr63uF5QqENL9P5c";
 
   @override
   Future<List<PlaceAutocomplete>> getAutocomplete(String searchInput) async {
     final String url =
-        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$searchInput&types=$types&key=$iosMapsAPI';
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$searchInput&types=$types&key=$mapsAPI';
 
     var response = await http.get(Uri.parse(url));
+    if (response.statusCode != 200) {
+      throw Exception("Failed to fetch data from api");
+    }
     var json = jsonDecode(response.body);
+    log(response.body);
     var results = json['predictions'] as List;
 
     return results.map((place) => PlaceAutocomplete.fromJson(place)).toList();
@@ -24,7 +29,7 @@ class PlacesRepository extends BasePlacesRepository {
   @override
   Future<Place> getPlace(String placeId) async {
     final String url =
-        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$iosMapsAPI';
+        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$mapsAPI';
 
     var response = await http.get(Uri.parse(url));
     var json = jsonDecode(response.body);
