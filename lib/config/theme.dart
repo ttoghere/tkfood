429b5ed3fda81252b513d5f82291f0df3875aa1e
ttoghere <tuncsoftwarehere@gmail.com>
2023-05-08 23:20:23 +0300

    Explanation Notes

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -4,10 +4,12 @@ ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: const Color(0xFFF5F5F5),
     colorScheme: const ColorScheme(
+      //Background Color
       primary: Color(0xFFFE3C5B),
       primaryContainer: Color(0xFFFE3C5B),
       secondary: Color(0xFFe84545),
       secondaryContainer: Color(0xFFe84545),
+      //Accent Color
       background: Color(0xFFFFFFFF),
       surface: Color(0xFFFFFFFF),
       error: Color(0xFF000000),
@@ -20,41 +22,49 @@ ThemeData theme() {
     ),
     fontFamily: 'Futura',
     textTheme: const TextTheme(
+      //Headline1
       displayLarge: TextStyle(
         color: Color(0xFF2b2e4a),
         fontWeight: FontWeight.bold,
         fontSize: 36,
       ),
+      //Headline2
       displayMedium: TextStyle(
         color: Color(0xFF2b2e4a),
         fontWeight: FontWeight.bold,
         fontSize: 24,
       ),
+      //Headline3
       displaySmall: TextStyle(
         color: Color(0xFF2b2e4a),
         fontWeight: FontWeight.bold,
         fontSize: 18,
       ),
+      //Headline4
       headlineMedium: TextStyle(
         color: Color(0xFF2b2e4a),
         fontWeight: FontWeight.bold,
         fontSize: 16,
       ),
+      //Headline5
       headlineSmall: TextStyle(
         color: Color(0xFF2b2e4a),
         fontWeight: FontWeight.bold,
         fontSize: 14,
       ),
+      //Headline6
       titleLarge: TextStyle(
         color: Color(0xFF2b2e4a),
         fontWeight: FontWeight.normal,
         fontSize: 14,
       ),
+      //BodyText1
       bodyLarge: TextStyle(
         color: Color(0xFF2b2e4a),
         fontWeight: FontWeight.normal,
         fontSize: 12,
       ),
+      //BodyText2
       bodyMedium: TextStyle(
         color: Color(0xFF2b2e4a),
         fontWeight: FontWeight.normal,
